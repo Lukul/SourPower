3ddacdcc9170351af9ab80792e792982b32a0e85
Lisann <lisann.gerdes@web.de>
2020-05-03 20:10:00 +0200

    Make buttons in profile fragment that change the theme

--- app/src/main/java/com/example/sourpower/MainActivity.java
+++ app/src/main/java/com/example/sourpower/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.sourpower;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,10 +16,13 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int THEME_FLOUR = 1;
+    private final static int THEME_FLOWER = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        updateTheme();
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -27,4 +35,40 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    public void updateTheme() {
+        if (ColorSchemeUtility.getTheme(getApplicationContext()) <= THEME_FLOUR) {
+            setTheme(R.style.AppThemeFlour);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDarkFlour));
+            }
+        } else if (ColorSchemeUtility.getTheme(getApplicationContext()) == THEME_FLOWER) {
+            setTheme(R.style.AppThemeFlower);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDarkFlower));
+            }
+        }
+    }
+
+
+    public void recreateActivity() {
+        Intent intent = getIntent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+    }
+
+    public void FlourThemeChange(View view) {
+        ColorSchemeUtility.setTheme(getApplicationContext(), 1);
+        recreateActivity();
+    }
+
+    public void FlowerThemeChange(View view) {
+        ColorSchemeUtility.setTheme(getApplicationContext(), 2);
+        recreateActivity();
+    }
 }
+
