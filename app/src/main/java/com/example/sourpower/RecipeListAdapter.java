8f7349aa370ba3c1664353ec9436a9f78a5bbe51
Lukul <lukasnolte@yahoo.de>
2020-05-31 14:44:33 +0200

    Clean up todo list and unused imports

--- app/src/main/java/com/example/sourpower/RecipeListAdapter.java
+++ app/src/main/java/com/example/sourpower/RecipeListAdapter.java
@@ -1,7 +1,6 @@
 package com.example.sourpower;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +12,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
 
 import java.util.LinkedList;
 
